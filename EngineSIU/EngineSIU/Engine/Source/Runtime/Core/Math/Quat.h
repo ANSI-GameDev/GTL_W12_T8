c0008fa94b87731774d6a1b0828dc368cd789269
정재현 <ray1203@naver.com>
2025-05-27 23:23:54 +0900

    Revert "Constraint 적절히 적용"
    This reverts commit 01d178cacdebfd63acf97fabd47b30f8bd1e48c0.

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Core/Math/Quat.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Core/Math/Quat.h
@@ -1,5 +1,4 @@
 #pragma once
-#include "Axis.h"
 #include "MathUtility.h"
 #include "Rotator.h"
 #include "Serialization/Archive.h"
@@ -163,7 +162,6 @@ public:
     //사용할거면 cpp에서 #include "Developer/PhysicsUtilities/PxConvertHelper.h"
     FQuat(const physx::PxQuat& InQuat);
     physx::PxQuat ToPxQuat();
-    FVector GetUnitAxis(EAxis::Type Axis) const;
 };
 
 inline FArchive& operator<<(FArchive& Ar, FQuat& Q)
